7b249ec1a30d3f1c730c1fd95fc8c3acf28e3df8
Yoz Grahame <yoz@yoz.com>
2020-12-17 14:54:39 -0800

    Yer standard Yeoman WebExtenstion bumf

---
+++ webextension-toolbox-config.js
@@ -0,0 +1,17 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: (config, { dev, vendor }) => {
+    // Perform customizations to webpack config
+    config.module.rules.push({
+        parser: {
+            amd: false
+        }
+    });        
+    // Important: return the modified config
+    return config;
+  }
+}
